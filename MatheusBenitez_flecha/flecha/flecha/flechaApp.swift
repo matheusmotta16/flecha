303d8d023c5dd80047b154af60973986dbbcf451
Matheus Motta <matheusmotta@10-44-48-53.0200.mack.br.iosda.org>
2025-07-14 13:21:39 -0300

    criei

---
+++ MatheusBenitez_flecha/flecha/flecha/flechaApp.swift
@@ -0,0 +1,17 @@
+//
+//  flechaApp.swift
+//  flecha
+//
+//  Created by Matheus Motta on 14/07/25.
+//
+
+import SwiftUI
+
+@main
+struct flechaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MatheusBenitez_flecha/flecha/flechaTests/flechaTests.swift
@@ -0,0 +1,17 @@
+//
+//  flechaTests.swift
+//  flechaTests
+//
+//  Created by Matheus Motta on 14/07/25.
+//
+
+import Testing
+@testable import flecha
+
+struct flechaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
